3b470b77faea9af5edc90f853c62feeba629c642
LucilaBal <95441877+LucilaBal@users.noreply.github.com>
2022-05-12 19:50:53 -0300

    Add files via upload

---
+++ src/components/ItemDetailContainer.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import customFetch from "../utils/CustomFetch";
+import ItemDetail from "./ItemDetail";
+import products from '../utils/Products';
+
+const ItemDetailContainer = () => {
+    const [dato, setDato] = useState({});
+    const {idItem} = useParams();
+    console.log(idItem);
+
+    useEffect(() => {
+        customFetch(2000, products.find(item =>item.id === parseInt(idItem)))
+            .then(result => setDato(result))
+            .catch(err => console.log(err))
+    }, []);
+    console.log(dato);
+    
+    return (
+        <ItemDetail item={dato} />
+    );
+}
+
+export default ItemDetailContainer;
---
+++ src/components/ItemList.js
@@ -0,0 +1,17 @@
+import Item from "./Item";
+import { ProductsContainer } from './StyledComponents';
+
+const ItemList = ({ items }) => {
+      //console.log("ItemList",items)
+    return (
+        <ProductsContainer>
+        {
+            items.length > 0
+            ? items.map(item => < Item id={item.id} Item key={item.id} title={item.name} price={item.cost} pictureUrl={item.image[0]} stock={item.stock} />)
+            : <p>Cargando...</p>
+        }
+        </ProductsContainer>
+    );
+}
+
+export default ItemList;
